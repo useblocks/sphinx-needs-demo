5edc6cc832b64b3f5094bfdf1b78a14f2f337290
Daniel Woste <daniel.woste@useblocks.com>
2024-02-12 23:56:52 +0100

    rtd template fix

--- docs/conf.py
+++ docs/conf.py
@@ -173,11 +173,15 @@ def rstjinja(app, docname, source):
     """
     Render our pages as a jinja template for fancy templating goodness.
     """
+    old_cwd = os.getcwd()
+    os.chdir(os.path.dirname(__file__))
     src = source[0]
     rendered = app.builder.templates.render_string(
         src, app.config.html_context
     )
     source[0] = rendered
+    os.chdir(old_cwd)
+
 
 def setup(app):
     app.connect("source-read", rstjinja)
